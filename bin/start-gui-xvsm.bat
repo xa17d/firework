c57441380ce89b95f68b012a48f6014e7c3f175b
Daniel Gehrer <daniel@xa1.at>
2014-11-28 22:16:52 +0100

    deactivate xvsm log

--- bin/start-gui-xvsm.bat
+++ bin/start-gui-xvsm.bat
@@ -1,1 +1,1 @@
-java -cp Gui.jar;ServiceInterface.jar;ServiceXvsm.jar;Actors.jar at.sbc.firework.gui.Main
+java -Dlogback.configurationFile=logback.xml -cp Gui.jar;ServiceInterface.jar;ServiceXvsm.jar;Actors.jar at.sbc.firework.gui.Main
